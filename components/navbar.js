359c4536d9b416f7afcfbd7139fe0bfdceed45b9
syedmuheed <syedmuheed@devsed.com>
2025-02-28 17:56:16 +0530

    hero section'=

--- components/navbar.js
+++ components/navbar.js
@@ -1,25 +1,31 @@
 import React from "react";
 import Padding from "./padding";
+import Image from "next/image";
+import logo from "@/public/images/logo.png"
+
 
 const Navbar = () => {
   return (
+    <div className=" sticky top-0">
     <Padding
       className={
-        " flex justify-center items-center text-[14px] h-[4rem] relative bg-white"
+        " flex justify-between items-center  h-[4rem] relative bg-white"
       }
     >
+        <div className=" h-[2rem] "><Image alt="logo" className=" h-full w-full " src={logo}/></div>
       <div className=" flex gap-8 justify-center items-center text-[#78777B]">
         <div>Home</div>
         <div>About</div>
         <div>Services</div>
         <div>Locations</div>
       </div>
-      <div className=" p-0.5 shadow-md rounded-full contactbtn absolute right-[1rem] md:right-[2rem] xl:right-[3rem] top-[50%] -translate-y-1/2 ">
-        <div className=" px-2 py-1  bg-white rounded-full ">
+      <div className=" p-[1.6px]   shadow-md rounded-full contactbtn  ">
+        <div className=" px-4 py-1.5  bg-white rounded-full ">
           <span className="linear-text-gradient">Contact us</span>
         </div>
       </div>
     </Padding>
+    </div>
   );
 };
 
