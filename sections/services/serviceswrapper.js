da3c29e49839bae19f7daa4653091157fda3ce60
SyedMuzammil2212 <syedsam2212@gmail.com>
2025-03-02 03:20:48 +0530

    Scroll Animation

--- sections/services/serviceswrapper.js
+++ sections/services/serviceswrapper.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Servicehero from "./servicehero";
 import Servicemarquee from "./servicemarquee";
+import Servicescroll from "./servicescroll";
 
 const Serviceswrapper = () => {
   return (
     <div className=" w-full bg-white ">
       <Servicehero />
       <Servicemarquee />
+      <Servicescroll />
+      <div className=" h-screen "></div>
     </div>
   );
 };
