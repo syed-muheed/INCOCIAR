5e9ab95105ed6726e0c2197b6f65103e67ceb02a
syedmuheed <syedmuheed@devsed.com>
2025-03-02 07:59:15 +0530

    kk

--- components/navbar.js
+++ components/navbar.js
@@ -13,15 +13,19 @@ const Navbar = () => {
         " flex justify-between items-center  h-[4rem] relative bg-white"
       }
     >
-        <div className=" h-[2rem] invisible "><Image alt="logo" className=" h-full w-full " src={logo}/></div>
-      <div className=" flex gap-8 justify-center items-center text-[#78777B]">
+        <div className=" h-[2rem]   invisible "><Image alt="logo" className=" h-full w-full " src={logo}/></div>
+      <div className=" md:flex gap-8 hidden justify-center items-center text-[#78777B]">
         <Link href={"/"}><div>Home</div></Link>
         <Link href={"/"}><div>About</div></Link>
         <Link href={"/services"}><div>Services</div></Link>
         <Link href={"/"}><div>Locations</div></Link>
         
       </div>
-      <div className=" p-[1.6px]   shadow-md rounded-full contactbtn  ">
+      <div className=" lg:hidden space-y-2">
+        <div className="w-[25px] h-[1.5px] rounded-full bg-black"></div>
+        <div className="w-[25px] h-[1.5px] rounded-full bg-black"></div>
+      </div>
+      <div className=" p-[1.6px] hidden lg:flex  shadow-md rounded-full contactbtn  ">
         <div className=" px-4 py-1.5  bg-white rounded-full ">
           <span className="linear-text-gradient">Contact us</span>
         </div>
--- sections/home/hero.js
+++ sections/home/hero.js
@@ -8,8 +8,8 @@ import { useEffect, useState } from "react";
 const Hero = () => {
     
   return (
-    <div className='relative top-[-5.5rem]'>
-      <div className=" flex justify-end px-[1rem]  md:px-[2rem] xl:px-[3rem]">
+    <div className='relative top-[1rem] md:top-[-5.5rem]'>
+      <div className=" flex justify-start md:justify-end px-[1rem]  md:px-[2rem] xl:px-[3rem]">
         <div className=" text-[17px] leading-[1.8rem] w-[24rem]">Transform your space into a reflection of who you are. At Incociar, we create interiors that enhance your lifestyle and inspire your daily moments.</div>
       </div>
       <div className=" pt-[2rem] "><Image alt="logo" className=" h-full w-full " src={hero}/></div>
