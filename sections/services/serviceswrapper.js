4f5fd2af29191db184b3831d30d7f3b261ca3dbb
SyedMuzammil2212 <syedsam2212@gmail.com>
2025-03-03 11:12:30 +0530

    push

--- sections/services/serviceswrapper.js
+++ sections/services/serviceswrapper.js
@@ -8,28 +8,31 @@ import Homebudget from "./homebudget";
 import Navigate from "./navigate";
 import Faqs from "./faqs";
 import Readytotransform from "../home/readytotransform";
+import Footer from "../home/footer";
 
 const Serviceswrapper = () => {
   return (
-    <div className=" w-full bg-white ">
-      {/* <Servicehero />
-      <Servicemarquee
-        q1={"BESPOKE LUXURY"}
-        q2={"HANDCRAFTED DETAILS"}
-        q3={"CURATED SPACES"}
-        q4={"CRAFTED PERFECTION"}
-        q5={"AESTHETIC LOOK"}
-        q6={"TAILORED DESIGNS"}
-      />
-      {/* <Servicescroll />
-      <Threebox /> */}
-      {/* <Homebudget />
-      <Navigate />
-      <Testimonials /> */}
-      <Faqs />
-      <Readytotransform />
-      <div className=" h-screen "></div>
-    </div>
+    <>
+      <div className=" w-full bg-white ">
+        <Servicehero />
+        <Servicemarquee
+          q1={"BESPOKE LUXURY"}
+          q2={"HANDCRAFTED DETAILS"}
+          q3={"CURATED SPACES"}
+          q4={"CRAFTED PERFECTION"}
+          q5={"AESTHETIC LOOK"}
+          q6={"TAILORED DESIGNS"}
+        />
+        <Servicescroll />
+        <Threebox />
+        <Homebudget />
+        <Navigate />
+        <Testimonials />
+        <Faqs />
+        <Readytotransform />
+        <Footer />
+      </div>
+    </>
   );
 };
 
