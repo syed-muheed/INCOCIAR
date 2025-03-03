7f8401810c40b6c205dcf3d24eee2eb6840f66f7
syedmuheed <syedmuheed@devsed.com>
2025-03-03 08:58:58 +0530

    kk

--- sections/home/why.js
+++ sections/home/why.js
@@ -10,8 +10,10 @@ const Why = () => {
           <div className=" h-[35%] flex">
             <div
               style={{ backgroundImage: "url(/images/why1.png)" }}
-              className=" bg-cover text-[#00000063] w-[35%] "
-            >Excellence in Every Detail</div>
+              className=" bg-cover text-[#00000046]  flex text-center pt-[1.5rem] justify-center text-[1.3rem] font-medium w-[35%] "
+            >
+              Excellence in Every Detail
+            </div>
             <div className=" bg-[#A3886E] text-[5rem] leading-[5rem] w-[65%] font-medium text-center flex items-center justify-center  text-[#7A2100] ">
               <div>
                 Why Choose <span className=" text-[white]"> Incociar ?</span>
@@ -21,20 +23,68 @@ const Why = () => {
           <div className="h-[65%] flex">
             <div
               style={{ backgroundImage: "url(/images/why3.png)" }}
-              className=" bg-cover text-black w-[60%] h-full "
-            >Personalized Approach</div>
+              className=" bg-cover flex relative items-center justify-center text-[2rem] font-medium text-black w-[60%] h-full "
+            ><div className="pt-4">Personalized Approach</div>
+                
+              
+              <div className=" absolute flex gap-2.5 top-5 right-5">
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Non-generic
+                </div>
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Unique
+                </div>
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Tailored
+                </div>
+              </div>
+            </div>
             <div
               style={{ backgroundImage: "url(/images/why4.png)" }}
-              className=" bg-cover text-[white] w-[40%] h-full"
-            >Quality Guaranteed</div>
+              className=" bg-cover text-[white] relative flex items-center justify-center text-[2rem] font-medium w-[40%] h-full"
+            ><div className="pt-4">Quality Guaranteed</div>
+              
+              <div className=" absolute flex gap-2.5 bottom-5 left-5">
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Premium
+                </div>
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Exceptional
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className=" h-full w-[26.5%] ">
           <div
             style={{ backgroundImage: "url(/images/why2.png)" }}
-            className=" bg-cover text-[white] h-[57%]"
-          >Expert Team</div>
-          <div className=" bg-[#7A2100] text-[white] h-[43%]">Timely Delivery</div>
+            className=" bg-cover text-[white] relative flex items-center justify-center text-[2rem] font-medium h-[57%]"
+          >
+            Expert Team
+            <div className=" absolute flex gap-2.5 bottom-5 left-5">
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Designers
+                </div>
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Experience
+                </div>
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Crafting
+                </div>
+              </div>
+          </div>
+          <div className=" bg-[#7A2100] text-[white] relative flex items-center justify-center text-[2rem] font-medium h-[43%]">
+            Timely Delivery
+            <div className=" absolute flex gap-2.5 top-5 left-5">
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Schedules
+                </div>
+                <div className=" text-[white] border border-[#c5c5c5] px-4 py-1.5 rounded-full text-[11px]">
+                  Deliver
+                </div>
+                
+              </div>
+          </div>
         </div>
       </div>
     </div>
